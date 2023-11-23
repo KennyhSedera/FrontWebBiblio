774a9dfadd7760b7d8033496fa4bfb8e0093ff11
KennyhSedera <kennyhsedera@gmail.com>
2023-11-23 17:01:24 +0300

    reservation7

--- src/views/ReservationView.jsx
+++ src/views/ReservationView.jsx
@@ -20,7 +20,23 @@ function ReservationView() {
                   <MdKeyboardArrowLeft size={25} style={{position:'absolute', left:10,}} />
                   <span style={{ fontSize: 20, fontWeight: 700 }}>Réservation</span>
               </div>
-              
+              <div style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'flex-start',
+                  justifyContent: 'space-between',
+              }}>
+                  <div style={{
+                    width:'50%', 
+                    background:'red', 
+                    minHeight:200,
+                  }}></div>
+                  <div style={{
+                    width:'50%', 
+                    background:'red', 
+                    minHeight:200,
+                  }}></div>
+              </div>
       </div>
     </Background>
   )
