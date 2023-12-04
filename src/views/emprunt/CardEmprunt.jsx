c1bb3a6d6575bff001ee30745cc95ff3e791ef11
KennyhSedera <kennyhsedera@gmail.com>
2023-12-04 10:17:11 +0300

    modale

--- src/views/emprunt/CardEmprunt.jsx
+++ src/views/emprunt/CardEmprunt.jsx
@@ -3,8 +3,10 @@ import './cardemprunt.css'
 import moment from 'moment'
 import { getImg } from '../../services/getImg'
 import Button from '../../components/Button'
+import Modal from '../../components/modal/Modal'
 
-function CardEmprunt({data}) {
+function CardEmprunt({ data }) {
+  const [open, setOpen]=React.useState(false)
   const str = data.adherent.prenom_Adh;
   const pseudo = str.split(' ');
   let date1 = new Date(data.retour_Emprunt);
@@ -34,8 +36,41 @@ function CardEmprunt({data}) {
             <div><strong>Retour: </strong>{ moment(data.retour_Emprunt).format('DD MMM YYYY ')}</div>
           </div>
           {date1 > date2 && <div style={{width:'60%', marginTop:20, marginLeft:'20%'}}>
-            <Button title='Retour' color='' textcolor='white' />
-          </div>}
+            <Button title='Retour' color='' textcolor='white' onClick={()=>setOpen(true)} />
+      </div>}
+      
+        <Modal open={open}>
+            <div style={{
+                background: '#ffffffe8', width: 380, minHeight: 100, padding: 10,
+                borderRadius: 15, color: 'black', fontSize: 18, fontWeight: 600,
+            }}>
+                <div style={{ marginLeft: 10, fontSize:22 }}>
+                    <span>Deconnexion ?</span>
+                </div>
+                <div style={{
+                    justifyContent: 'center', display: 'flex',
+                    height: 50, alignItems: 'center', fontWeight: 500,
+                    marginBottom: 20, fontSize: 15,
+                }}>
+                    <span>Voulez-vous se deconnecter vraiment ?</span>
+                </div>
+                <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+                    <div style={{ width: '42%', }}>
+                        <Button small onClick={() => setOpen(false)} title='annuler' color='white' textcolor='black' textsize={15} />
+                    </div>
+                    <div to='/' style={{ width: '42%', textDecoration: 'none' }}>
+                        <Button
+                            small
+                            title='Deconnecter'
+                            color='#00b2fee1'
+                            textcolor='white'
+                            textsize={15}
+                            onClick={handleLogout}
+                        />
+                    </div>
+                </div>
+            </div>
+        </Modal>
     </div>
   )
 }
