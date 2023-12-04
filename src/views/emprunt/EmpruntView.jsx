6254baccb51d5ab6c1715ae096defc75bf59321f
KennyhSedera <kennyhsedera@gmail.com>
2023-12-04 10:42:09 +0300

    modalRetour

--- src/views/emprunt/EmpruntView.jsx
+++ src/views/emprunt/EmpruntView.jsx
@@ -52,40 +52,6 @@ function EmpruntView() {
     setOpenRetour(true)
     const str = item.adherent.prenom_Adh;
     const pseudo = str.split(' ');
-    return (
-      <Modal open={openRetour}>
-        <div style={{
-            background: '#ffffffe8', width: 380, minHeight: 100, padding: 10,
-            borderRadius: 15, color: 'black', fontSize: 18, fontWeight: 600,
-        }}>
-            <div style={{ marginLeft: 10, fontSize:22 }}>
-                <span>Retour Emprunt ?</span>
-            </div>
-            <div style={{
-                justifyContent: 'center', display: 'flex',
-                height: 50, alignItems: 'center', fontWeight: 500,
-                marginBottom: 20, fontSize: 15,
-            }}>
-                <span>Voulez-vous enregistrer la retour du {item.livre.titre_livre} emprunté par {pseudo} le {moment(item.date_Emprunt).format('DD MMM YYYY ')} ?</span>
-            </div>
-            <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-                <div style={{ width: '42%', }}>
-                    <Button small onClick={() => setOpenRetour(false)} title='annuler' color='white' textcolor='black' textsize={15} />
-                </div>
-                <div to='/' style={{ width: '42%', textDecoration: 'none' }}>
-                    <Button
-                        small
-                        title='Enregistrer'
-                        color='#00b2fee1'
-                        textcolor='white'
-                        textsize={15}
-                        // onClick={handleLogout}
-                    />
-                </div>
-            </div>
-        </div>
-      </Modal>
-    )
   }
   return (
     <MainLayout 
@@ -134,6 +100,38 @@ function EmpruntView() {
         <EmpruntBook onClose={()=>{setIsOpen(false); getAll()}} title='Emprunter' />
       </Modal>
       
+      <Modal open={openRetour}>
+        <div style={{
+            background: '#ffffffe8', width: 380, minHeight: 100, padding: 10,
+            borderRadius: 15, color: 'black', fontSize: 18, fontWeight: 600,
+        }}>
+            <div style={{ marginLeft: 10, fontSize:22 }}>
+                <span>Retour Emprunt ?</span>
+            </div>
+            <div style={{
+                justifyContent: 'center', display: 'flex',
+                height: 50, alignItems: 'center', fontWeight: 500,
+                marginBottom: 20, fontSize: 15,
+            }}>
+                {/* <span>Voulez-vous enregistrer la retour du {item.livre.titre_livre} emprunté par {pseudo} le {moment(item.date_Emprunt).format('DD MMM YYYY ')} ?</span> */}
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+                <div style={{ width: '42%', }}>
+                    <Button small onClick={() => setOpenRetour(false)} title='annuler' color='white' textcolor='black' textsize={15} />
+                </div>
+                <div to='/' style={{ width: '42%', textDecoration: 'none' }}>
+                    <Button
+                        small
+                        title='Enregistrer'
+                        color='#00b2fee1'
+                        textcolor='white'
+                        textsize={15}
+                        // onClick={handleLogout}
+                    />
+                </div>
+            </div>
+        </div>
+      </Modal>
     </MainLayout>
   )
 }
